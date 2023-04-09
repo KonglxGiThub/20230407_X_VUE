3baf68593e819b8086f874fb58eb35929c7536cb
konglxgitee <konglxgithub@163.com>
2023-04-09 15:40:51 +0800

    路由配置和使用

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <!-- 路由出口 -->
+      <!-- 路由匹配到的组件将渲染在这里 -->
+      <router-view></router-view>
+    </el-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
---
+++ my-app/src/views/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <!-- 子路由出口 -->
+          <!-- 子路由匹配到的组件将渲染在这里 -->
+          <router-view></router-view
+        ></el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+ <script>
+export default {
+  //name:"Home",
+  data() {
+    return {};
+  },
+};
+</script>
